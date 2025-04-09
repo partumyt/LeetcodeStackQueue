0716c0883e0075b16b4e9a3aef1f91696ad5e324
Nazar Mykhailyshchuk <partumyt@gmail.com>
2025-04-09 14:40:29 +0300

    3.1

--- 3.py
+++ 3.py
@@ -3,21 +3,32 @@ from collections import Counter
 class FreqStack(object):
 
     def __init__(self):
-        self.stack = []
+        self.freq = {}
+        self.group = {}
+        self.maxfreq = 0
 
     def push(self, val):
         """
         :type val: int
         :rtype: None
         """
-        self.stack.append(val)
+        if val in self.freq:
+            self.freq[val] += 1
+        else:
+            self.freq[val] = 1
+        f = self.freq[val]
+        if f > self.maxfreq:
+            self.maxfreq = f
+        if f not in self.group:
+            self.group[f] = []
+        self.group[f].append(val)
 
     def pop(self):
         """
         :rtype: int
         """
-        freq = Counter(self.stack)
-        max_freq = max(freq.values())
-        for i in range(len(self.stack) - 1, -1, -1):
-            if freq[self.stack[i]] == max_freq:
-                return self.stack.pop(i)
+        val = self.group[self.maxfreq].pop()
+        self.freq[val] -= 1
+        if not self.group[self.maxfreq]:
+            self.maxfreq -= 1
+        return val
